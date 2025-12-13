06811378ed6889b5c8f8dbe1100c089e6f157f81
Abhinand k <abhinnanndk@gmail.com>
2025-12-13 10:01:02 +0530

    unique google id key error fixed

--- model/userSchema.js
+++ model/userSchema.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
     googleId: {
         type: String,
         unique: true,
-        // sparse: true // if not all users have this
+        sparse: true
     },
     password: {
         type: String,
